1e51a46626fae52c9af1620276b784984af85bfb
darksystem23 <aaron.glez.diaz.91@gmail.com>
2016-03-15 16:57:43 +0000

    U - Script Update Ubuntu

--- update-ubuntu.sh
+++ update-ubuntu.sh
@@ -2,16 +2,22 @@
 
 # Script - Update Repositories, Upgrade System, Update Distribution
 
+# Update Repositories Ubuntu
 sudo apt-get -y update
 sudo apt-get -y update
 
+# Upgrade Dependencies
 sudo apt-get -y upgrade
 
+# Upgrade Distribution Ubuntu
 sudo apt-get -y dist-upgrade
 
+# Check Dependencies retained or broken
 sudo apt-get -y install -f
 
+# Uninstall dependencies that are not in use
 sudo apt-get -y autoremove
 
+# Clean cache apt
 sudo apt-get -y clean
 
